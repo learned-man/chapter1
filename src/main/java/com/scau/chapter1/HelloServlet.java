ea4205311d2ec5a03a4f3d18437c9e9fd9fabd76
shusheng <1040326000@qq.com>
2018-01-07 14:39:13 +0800

    test

---
+++ src/main/java/com/scau/chapter1/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.scau.chapter1;
+
+
+
+import java.text.DateFormat;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        System.out.println(currentTime);
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/jsp/hello.jsp").forward(req,resp);
+    }
+
+}
---
+++ src/main/webapp/jsp/hello.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 吴王冲
+  Date: 2018-01-03
+  Time: 19:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page pageEncoding="utf-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt"%>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    <h1>Hello!</h1>
+
+    <h2>当前时间:${currentTime}</h2>
+</body>
+</html>
